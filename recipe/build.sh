b500234974c22e8dd4e4d061a3c0c85d08928de6
chakpongchung <chakpongchung@gmail.com>
2021-12-07 14:47:24 -0500

    wip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,19 +2,25 @@ rm -rf build
 mkdir build
 cd build
 
-cmake -B . -S .. -Wno-dev
+CPU_COUNT=4
+PREFIX=4
+echo ${CPU_COUNT}
+echo ${PREFIX}
 
-make -j${CPU_COUNT}
+# cmake -B . -S .. -Wno-dev
+# make -j${CPU_COUNT}
 
-# Install manually since the build files don't have an install target
-mkdir -p ${PREFIX}/bin
-cp bliss ${PREFIX}/bin
 
-mkdir -p ${PREFIX}/lib
-cp libbliss* ${PREFIX}/lib
 
-mkdir -p ${PREFIX}/include/bliss
-cp ../src/*.hh ${PREFIX}/include/bliss
+# # Install manually since the build files don't have an install target
+# mkdir -p ${PREFIX}/bin
+# cp bliss ${PREFIX}/bin
 
-# Patch the header files so use bliss/ prefix
-sed -i 's@#include "\(.*\)"$@#include <bliss/\1>@' ${PREFIX}/include/bliss/*
+# mkdir -p ${PREFIX}/lib
+# cp libbliss* ${PREFIX}/lib
+
+# mkdir -p ${PREFIX}/include/bliss
+# cp ../src/*.hh ${PREFIX}/include/bliss
+
+# # Patch the header files so use bliss/ prefix
+# sed -i 's@#include "\(.*\)"$@#include <bliss/\1>@' ${PREFIX}/include/bliss/*
